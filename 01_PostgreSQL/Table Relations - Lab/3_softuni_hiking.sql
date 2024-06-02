f9e3ae51a0202b20fcb0a6203835c8fabb246473
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-02 19:00:24 +0300

    added solution

---
+++ 01_PostgreSQL/Table Relations - Lab/3_softuni_hiking.sql
@@ -0,0 +1,8 @@
+SELECT 
+	r.start_point,
+    r.end_point,
+    r.leader_id,
+       concat_ws(' ', c.first_name, c.last_name) AS leader_name
+FROM 
+	routes r
+    	JOIN campers c ON c.id = r.leader_id;

296d2dccc35d05417abc56be9277799b3414f1ab
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-11 19:17:08 +0300

    added solution

---
+++ 01_PostgreSQL/Subqueries and JOINs - Exercise/02_first_10_apartments_booked_at.sql
@@ -0,0 +1,22 @@
+SELECT 
+	a.name,
+	a.country,
+	b.booked_at::DATE
+FROM 
+	apartments AS a
+LEFT JOIN 
+	bookings AS b
+USING (booking_id)
+LIMIT 10;
+
+
+-- SELECT 
+-- 	a.name,
+-- 	a.country,
+-- 	b.booked_at::DATE
+-- FROM 
+-- 	apartments AS a
+-- LEFT JOIN 
+-- 	bookings AS b
+-- ON b.booking_id = a.booking_id
+-- LIMIT 10;

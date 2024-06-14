90632c07c2d16b75c46a317e7e16bda62d74a7be
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-14 10:40:21 +0300

    Update 07_bookings_made_by_lead..sql

--- 01_PostgreSQL/Subqueries and JOINs - Exercise/07_bookings_made_by_lead..sql
+++ 01_PostgreSQL/Subqueries and JOINs - Exercise/07_bookings_made_by_lead..sql
@@ -1,9 +1,27 @@
-SELECT 
-	b.apartment_id,
-	b.booked_for,
-	c.first_name,
-	c.country
-FROM 
-	customers AS c
-    JOIN bookings b USING (customer_id)
-WHERE c.job_type = 'Lead';
+SELECT
+    b.apartment_id,
+    b.booked_for,
+    c.first_name,
+    c.country
+FROM
+    bookings AS b
+JOIN
+    customers AS c
+USING
+    (customer_id)
+WHERE
+    c.job_type LIKE '%Lead%'
+;
+
+
+
+
+-- SELECT 
+-- 	b.apartment_id,
+-- 	b.booked_for,
+-- 	c.first_name,
+-- 	c.country
+-- FROM 
+-- 	customers AS c
+--     JOIN bookings b USING (customer_id)
+-- WHERE c.job_type = 'Lead';

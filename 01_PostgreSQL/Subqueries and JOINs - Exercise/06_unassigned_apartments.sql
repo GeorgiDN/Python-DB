a7c5ad48ba946fd5701af491be304fce7a7c003e
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-14 10:20:03 +0300

    Update 06_unassigned_apartments.sql

--- 01_PostgreSQL/Subqueries and JOINs - Exercise/06_unassigned_apartments.sql
+++ 01_PostgreSQL/Subqueries and JOINs - Exercise/06_unassigned_apartments.sql
@@ -1,12 +1,28 @@
-SELECT 
-	b.booking_id,
-	b.apartment_id,
-	c.companion_full_name
-FROM 
-	bookings AS b
-JOIN 
-	customers AS c
-USING 
-	(customer_id)
-WHERE 
-	apartment_id IS NULL;
+SELECT
+    b.booking_id,
+    b.apartment_id,
+    c.companion_full_name
+FROM
+    bookings AS b
+JOIN
+    customers AS c
+USING
+    (customer_id)
+WHERE
+    b.apartment_id IS NULL
+;
+
+
+
+-- SELECT 
+-- 	b.booking_id,
+-- 	b.apartment_id,
+-- 	c.companion_full_name
+-- FROM 
+-- 	bookings AS b
+-- JOIN 
+-- 	customers AS c
+-- USING 
+-- 	(customer_id)
+-- WHERE 
+-- 	apartment_id IS NULL;

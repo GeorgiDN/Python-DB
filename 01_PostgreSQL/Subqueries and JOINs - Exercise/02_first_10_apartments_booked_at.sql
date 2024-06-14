2180089965b3621fdb13ba4b0337788998942592
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-14 09:10:58 +0300

    Update 02_first_10_apartments_booked_at.sql

--- 01_PostgreSQL/Subqueries and JOINs - Exercise/02_first_10_apartments_booked_at.sql
+++ 01_PostgreSQL/Subqueries and JOINs - Exercise/02_first_10_apartments_booked_at.sql
@@ -1,13 +1,15 @@
-SELECT 
-	a.name,
-	a.country,
-	b.booked_at::DATE
-FROM 
-	apartments AS a
-LEFT JOIN 
-	bookings AS b
-USING (booking_id)
-LIMIT 10;
+SELECT
+    a.name,
+    a.country,
+    b.booked_at::DATE
+FROM
+    apartments AS a
+LEFT JOIN
+    bookings AS b
+USING
+    (booking_id)
+LIMIT 10
+;
 
 
 -- SELECT 
@@ -20,3 +22,19 @@ LIMIT 10;
 -- 	bookings AS b
 -- ON b.booking_id = a.booking_id
 -- LIMIT 10;
+
+
+
+
+-- SELECT
+--     a.name,
+--     a.country,
+--     b.booked_at::DATE
+-- FROM
+--     bookings AS b
+-- RIGHT JOIN
+--     apartments AS a
+-- USING
+--     (booking_id)
+-- LIMIT 10
+-- ;

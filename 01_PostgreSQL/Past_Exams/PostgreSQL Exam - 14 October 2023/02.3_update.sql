56111d901b22efe9eac6220304c1b99d82f9f812
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-06-28 07:38:14 +0300

    added more solutions

---
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Exam - 14 October 2023/02.3_update.sql
@@ -0,0 +1,23 @@
+UPDATE coaches
+SET salary = salary * coach_level
+WHERE first_name LIKE 'C%'
+AND EXISTS (
+    SELECT 1 
+    FROM players_coaches 
+    WHERE players_coaches.coach_id = coaches.id
+);
+
+
+
+-- UPDATE coaches
+-- SET salary = salary * coach_level
+-- WHERE first_name LIKE 'C%';
+
+
+
+-- UPDATE coaches
+-- SET salary = salary * coach_level
+-- WHERE first_name LIKE 'C%'
+--   AND id IN (SELECT DISTINCT coach_id
+--              FROM players_coaches
+--              WHERE coach_id IS NOT NULL);

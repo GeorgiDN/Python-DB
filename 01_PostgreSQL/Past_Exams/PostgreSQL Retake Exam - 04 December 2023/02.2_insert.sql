db1c4feaaa69fbbef37786221e422f2d97cc81b1
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-01 07:52:08 +0300

    added more solutions

---
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Retake Exam - 04 December 2023/02.2_insert.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS gift_recipients (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    country_id INT,
+    gift_sent BOOLEAN DEFAULT FALSE
+);
+
+INSERT INTO gift_recipients(name, country_id, gift_sent)
+(SELECT
+    CONCAT(c.first_name, ' ', c.last_name),
+    c.country_id,
+    CASE WHEN c.country_id IN (7, 8, 14, 17, 26) THEN TRUE
+    ELSE FALSE
+    END
+FROM
+    customers AS c
+);
+
+
+
+
+-- CREATE TABLE IF NOT EXISTS gift_recipients (
+--   id SERIAL PRIMARY KEY,
+--   name VARCHAR(100) NOT NULL,
+--   country_id INT NOT NULL,
+--   gift_sent BOOLEAN DEFAULT FALSE
+-- );
+
+
+-- INSERT INTO gift_recipients (
+-- 	name, country_id, gift_sent
+-- )
+-- SELECT
+--   CONCAT(first_name, ' ', last_name) AS name,
+--   country_id AS country_id,
+--   CASE
+--     WHEN country_id IN (7, 8, 14, 17, 26) THEN TRUE
+--     ELSE FALSE
+--   END AS gift_sent
+-- FROM customers;
---
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Retake Exam - 04 December 2023/02.3_update.sql
@@ -0,0 +1,20 @@
+UPDATE products
+SET price = price * 1.1
+FROM feedbacks
+WHERE feedbacks.rate > 8;
+
+
+
+-- UPDATE products
+-- SET price = price * 1.10
+-- WHERE id IN (
+--    SELECT product_id
+--    FROM feedbacks
+--    WHERE rate > 8
+-- );
+
+
+
+-- UPDATE products
+-- SET price = price * 1.1
+-- WHERE id IN (SELECT product_id FROM feedbacks WHERE rate > 8);

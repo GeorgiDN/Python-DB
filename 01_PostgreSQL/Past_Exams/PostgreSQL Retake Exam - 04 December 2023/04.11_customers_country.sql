e7745b6317240a255e9b9570592b0cb90228a589
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-02 18:25:44 +0300

    added more solutions

---
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Retake Exam - 04 December 2023/04.11_customers_country.sql
@@ -0,0 +1,46 @@
+CREATE OR REPLACE PROCEDURE sp_customer_country_name(
+    IN customer_full_name VARCHAR (50),
+    OUT country VARCHAR (50)
+) AS
+$$
+BEGIN
+    SELECT c.name
+    INTO country
+    FROM countries AS c
+    JOIN customers AS cus
+    ON c.id = cus.country_id
+    WHERE CONCAT(cus.first_name, ' ', cus.last_name) = customer_full_name;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+-- CALL sp_customer_country_name('Betty Wallace', '');
+-- CALL sp_customer_country_name('Rachel Bishop', '');
+-- CALL sp_customer_country_name('Jerry Andrews', '');
+
+
+
+
+
+-- CREATE OR REPLACE PROCEDURE sp_customer_country_name(
+--     IN customer_full_name VARCHAR(50),
+--     OUT country_name VARCHAR(50)
+-- )
+-- AS
+-- $$
+-- BEGIN
+--     SELECT coun.name
+--     INTO country_name
+--     FROM countries AS coun
+--              JOIN customers AS cus
+--                   ON coun.id = cus.country_id
+--     WHERE customer_full_name = CONCAT(cus.first_name, ' ', cus.last_name);
+-- END;
+-- $$
+--     LANGUAGE plpgsql;
+
+-- -- CALL sp_customer_country_name('Betty Wallace', '');
+-- -- CALL sp_customer_country_name('Rachel Bishop', '');
+-- -- CALL sp_customer_country_name('Jerry Andrews', '');
+

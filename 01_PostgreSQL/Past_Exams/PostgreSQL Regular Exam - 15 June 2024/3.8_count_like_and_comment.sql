640f94a84e5f09b8fdba5b8a748160da657c28f9
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-13 17:42:37 +0300

    Update 3.8_count_like_and_comment.sql

--- 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/3.8_count_like_and_comment.sql
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/3.8_count_like_and_comment.sql
@@ -1,21 +1,63 @@
-SELECT p.id                          AS photo_id,
-       COALESCE(l.likes_count, 0)    AS likes_count,
-       COALESCE(c.comments_count, 0) AS comments_count
-FROM photos AS p
-         LEFT JOIN (SELECT photo_id,
-                           COUNT(*) AS likes_count
-                    FROM likes
-                    GROUP BY photo_id) l ON p.id = l.photo_id
-         LEFT JOIN (SELECT photo_id,
-                           COUNT(*) AS comments_count
-                    FROM comments
-                    GROUP BY photo_id) c ON p.id = c.photo_id
-ORDER BY likes_count DESC,
-         comments_count DESC,
-         photo_id;
+WITH likes_count AS (
+    SELECT
+        photo_id,
+        COUNT(id) AS likes_count
+    FROM
+        likes
+    GROUP BY
+        photo_id
+),
+comments_count AS (
+    SELECT
+        photo_id,
+        COUNT(id) AS comments_count
+    FROM
+        comments
+    GROUP BY
+        photo_id
+)
+SELECT
+    p.id AS photo_id,
+    COALESCE(lc.likes_count, 0) AS likes_count,
+    COALESCE(cc.comments_count, 0) AS comments_count
+FROM
+    photos AS p
+LEFT JOIN
+    likes_count AS lc
+ON
+    p.id = lc.photo_id
+LEFT JOIN
+    comments_count AS cc
+ON
+    p.id = cc.photo_id
+ORDER BY
+    likes_count DESC,
+    comments_count DESC,
+    p.id;
+
+
+
+
+-- SELECT p.id                          AS photo_id,
+--        COALESCE(l.likes_count, 0)    AS likes_count,
+--        COALESCE(c.comments_count, 0) AS comments_count
+-- FROM photos AS p
+--          LEFT JOIN (SELECT photo_id,
+--                            COUNT(*) AS likes_count
+--                     FROM likes
+--                     GROUP BY photo_id) l ON p.id = l.photo_id
+--          LEFT JOIN (SELECT photo_id,
+--                            COUNT(*) AS comments_count
+--                     FROM comments
+--                     GROUP BY photo_id) c ON p.id = c.photo_id
+-- ORDER BY likes_count DESC,
+--          comments_count DESC,
+--          photo_id;
 		 
 		 
-		 
+
+
+
 -- SELECT p.id                          AS photo_id,
 --        COALESCE(l.likes_count, 0)    AS likes_count,
 --        COALESCE(c.comments_count, 0) AS comments_count

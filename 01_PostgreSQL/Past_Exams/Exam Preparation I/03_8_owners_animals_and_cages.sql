879c0d16c9368d5ad53af17cafec3f15c03493f0
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-20 21:56:21 +0300

    Update 03_8_owners_animals_and_cages.sql

--- 01_PostgreSQL/Past_Exams/Exam Preparation I/03_8_owners_animals_and_cages.sql
+++ 01_PostgreSQL/Past_Exams/Exam Preparation I/03_8_owners_animals_and_cages.sql
@@ -1,5 +1,5 @@
 SELECT
-    CONCAT(o.name, ' - ',a.name) AS "owners - animals",
+    CONCAT_WS(' - ', o.name, a.name)  AS "owners - animals",
     o.phone_number,
     ac.cage_id
 FROM
@@ -9,19 +9,47 @@ JOIN
 ON
     o.id = a.owner_id
 JOIN
-    animals_cages AS ac
-ON
-    a.id = ac.animal_id
-JOIN
-    cages AS c
-ON
-    ac.cage_id = c.id
-JOIN
     animal_types AS at
 ON
     a.animal_type_id = at.id
+JOIN
+    animals_cages AS ac
+ON
+    a.id = ac.animal_id
 WHERE
-	at.animal_type = 'Mammals'
+    at.animal_type LIKE 'Mammals'
 ORDER BY
     o.name,
-    a.name DESC
+    a.name DESC;
+
+
+
+
+
+-- SELECT
+--     CONCAT(o.name, ' - ',a.name) AS "owners - animals",
+--     o.phone_number,
+--     ac.cage_id
+-- FROM
+--     owners AS o
+-- JOIN
+--     animals AS a
+-- ON
+--     o.id = a.owner_id
+-- JOIN
+--     animals_cages AS ac
+-- ON
+--     a.id = ac.animal_id
+-- JOIN
+--     cages AS c
+-- ON
+--     ac.cage_id = c.id
+-- JOIN
+--     animal_types AS at
+-- ON
+--     a.animal_type_id = at.id
+-- WHERE
+-- 	at.animal_type = 'Mammals'
+-- ORDER BY
+--     o.name,
+--     a.name DESC

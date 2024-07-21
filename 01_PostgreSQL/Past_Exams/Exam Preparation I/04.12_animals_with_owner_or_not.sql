491ca54b9ae9254a2f349c062be4f101581fec37
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-21 09:45:30 +0300

    Update 04.12_animals_with_owner_or_not.sql

--- 01_PostgreSQL/Past_Exams/Exam Preparation I/04.12_animals_with_owner_or_not.sql
+++ 01_PostgreSQL/Past_Exams/Exam Preparation I/04.12_animals_with_owner_or_not.sql
@@ -21,10 +21,34 @@ END;
 $$;
 
 
-CALL sp_animals_with_owners_or_not('Pumpkinseed Sunfish', '');
+-- CALL sp_animals_with_owners_or_not('Pumpkinseed Sunfish', '');
 -- CALL sp_animals_with_owners_or_not('Hippo', '');
 -- CALL sp_animals_with_owners_or_not('Brown bear', '');
 
+
+
+---------------------------------------------------------------------------------
+--     CREATE OR REPLACE PROCEDURE sp_animals_with_owners_or_not(
+--     IN animal_name VARCHAR (30),
+--     OUT owner_name VARCHAR (40)
+-- )
+-- AS
+-- $$
+-- BEGIN
+--     SELECT COALESCE(o.name, 'For adoption')
+--     INTO owner_name
+--     FROM owners AS o
+--     RIGHT JOIN animals AS a
+--     ON o.id = a.owner_id
+--     WHERE animal_name LIKE a.name;
+-- END;
+-- $$
+-- LANGUAGE plpgsql;
+
+
+
+
+-----------------------------------------------------------------------------
 
 
 -- -- Stored Procedure which return NOTICE

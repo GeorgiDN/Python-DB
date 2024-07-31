6a0a5d7b07e3cb77358d5493513ebad09c6f364f
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-31 09:35:21 +0300

    Update 04.10_customer_feedback.sql

--- 01_PostgreSQL/Past_Exams/PostgreSQL Retake Exam - 04 December 2023/04.10_customer_feedback.sql
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Retake Exam - 04 December 2023/04.10_customer_feedback.sql
@@ -1,45 +1,79 @@
-CREATE OR REPLACE FUNCTION fn_feedbacks_for_product(
-    product_name VARCHAR(25)
+DROP FUNCTION IF EXISTS fn_feedbacks_for_product;
+CREATE FUNCTION fn_feedbacks_for_product(
+    IN product_name VARCHAR (25)
 )
-    RETURNS TABLE
-            (
-                customer_id          INT,
-                customer_name        VARCHAR(75),
-                feedback_description VARCHAR(255),
-                feedback_rate        NUMERIC(4, 2)
-            )
-AS
+RETURNS TABLE(
+    customer_id INT,
+    customer_name VARCHAR (75),
+    feedback_description VARCHAR(255),
+    feedback_rate NUMERIC (4, 2)
+) AS
 $$
 BEGIN
     RETURN QUERY
-        (SELECT c.id,
-                c.first_name,
-                f.description,
-                f.rate
-         FROM feedbacks AS f
-                  JOIN customers AS c
-                       ON f.customer_id = c.id
-                  JOIN products AS p
-                       ON f.product_id = p.id
-         WHERE p.name = product_name
-         ORDER BY f.customer_id);
+    SELECT
+        c.id,
+        c.first_name,
+        f.description,
+        f.rate
+    FROM
+        customers AS c
+    JOIN
+        feedbacks AS f
+    ON
+        c.id = f.customer_id
+    JOIN
+        products AS p
+    ON
+        f.product_id = p.id
+    WHERE
+        p.name = product_name
+    ORDER BY
+        c.id;
 END;
 $$
-    LANGUAGE plpgsql;
+LANGUAGE plpgsql;
 
-
--- SELECT *
--- FROM fn_feedbacks_for_product('Banitsa');
---
--- SELECT *
--- FROM fn_feedbacks_for_product('ALCOHOL');
-
+-- SELECT * FROM fn_feedbacks_for_product('Banitsa');
+-- SELECT * FROM fn_feedbacks_for_product('ALCOHOL');
 -- SELECT * FROM fn_feedbacks_for_product('Bread');
 
 
 
 
+----------------------------------------------------------------------
+-- CREATE OR REPLACE FUNCTION fn_feedbacks_for_product(
+--     product_name VARCHAR(25)
+-- )
+--     RETURNS TABLE
+--             (
+--                 customer_id          INT,
+--                 customer_name        VARCHAR(75),
+--                 feedback_description VARCHAR(255),
+--                 feedback_rate        NUMERIC(4, 2)
+--             )
+-- AS
+-- $$
+-- BEGIN
+--     RETURN QUERY
+--         (SELECT c.id,
+--                 c.first_name,
+--                 f.description,
+--                 f.rate
+--          FROM feedbacks AS f
+--                   JOIN customers AS c
+--                        ON f.customer_id = c.id
+--                   JOIN products AS p
+--                        ON f.product_id = p.id
+--          WHERE p.name = product_name
+--          ORDER BY f.customer_id);
+-- END;
+-- $$
+--     LANGUAGE plpgsql;
 
+
+
+------------------------------------------------------------------------
 -- CREATE OR REPLACE FUNCTION fn_feedbacks_for_product(
 --     product_name VARCHAR(25)
 -- ) RETURNS TABLE (
@@ -67,8 +101,3 @@ $$
 -- END;
 -- $$
 -- LANGUAGE plpgsql;
-
-
--- -- SELECT * FROM fn_feedbacks_for_product('Banitsa');
--- -- SELECT * FROM fn_feedbacks_for_product('ALCOHOL');
--- -- SELECT * FROM fn_feedbacks_for_product('Bread');

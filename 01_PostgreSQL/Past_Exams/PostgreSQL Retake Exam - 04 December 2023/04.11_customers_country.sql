11e6a9fc79a2702533dc9cfb0917481da9fa8f63
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-07-31 09:33:32 +0300

    Update 04.11_customers_country.sql

--- 01_PostgreSQL/Past_Exams/PostgreSQL Retake Exam - 04 December 2023/04.11_customers_country.sql
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Retake Exam - 04 December 2023/04.11_customers_country.sql
@@ -1,15 +1,21 @@
-CREATE OR REPLACE PROCEDURE sp_customer_country_name(
+CREATE PROCEDURE sp_customer_country_name(
     IN customer_full_name VARCHAR (50),
-    OUT country VARCHAR (50)
+    OUT country_name VARCHAR (50)
 ) AS
 $$
 BEGIN
-    SELECT c.name
-    INTO country
-    FROM countries AS c
-    JOIN customers AS cus
-    ON c.id = cus.country_id
-    WHERE CONCAT(cus.first_name, ' ', cus.last_name) = customer_full_name;
+    SELECT
+        cou.name
+    INTO
+        country_name
+    FROM
+        countries AS cou
+    JOIN
+        customers AS cus
+    ON
+        cou.id = cus.country_id
+    WHERE
+        CONCAT(cus.first_name, ' ', cus.last_name) LIKE customer_full_name;
 END;
 $$
 LANGUAGE plpgsql;
@@ -20,9 +26,25 @@ LANGUAGE plpgsql;
 -- CALL sp_customer_country_name('Jerry Andrews', '');
 
 
+----------------------------------------------------------------------------
+-- CREATE OR REPLACE PROCEDURE sp_customer_country_name(
+--     IN customer_full_name VARCHAR (50),
+--     OUT country VARCHAR (50)
+-- ) AS
+-- $$
+-- BEGIN
+--     SELECT c.name
+--     INTO country
+--     FROM countries AS c
+--     JOIN customers AS cus
+--     ON c.id = cus.country_id
+--     WHERE CONCAT(cus.first_name, ' ', cus.last_name) = customer_full_name;
+-- END;
+-- $$
+-- LANGUAGE plpgsql;
 
 
-
+----------------------------------------------------------------------------
 -- CREATE OR REPLACE PROCEDURE sp_customer_country_name(
 --     IN customer_full_name VARCHAR(50),
 --     OUT country_name VARCHAR(50)

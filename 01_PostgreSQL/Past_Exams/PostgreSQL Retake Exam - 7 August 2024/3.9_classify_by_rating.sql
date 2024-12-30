18b24207a8926367b254dfca9790ec025f054bf4
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2024-12-30 08:12:29 +0200

    Update 3.9_classify_by_rating.sql

--- 01_PostgreSQL/Past_Exams/PostgreSQL Retake Exam - 7 August 2024/3.9_classify_by_rating.sql
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Retake Exam - 7 August 2024/3.9_classify_by_rating.sql
@@ -2,8 +2,8 @@ SELECT
     p.title,
     CASE
         WHEN pi.rating <= 3.50 THEN 'poor'
-        WHEN pi.rating BETWEEN 3.51 AND 8.0 THEN 'good'
-        ELSE 'excellent'
+        WHEN pi.rating BETWEEN 3.51 AND 8.00 THEN 'good'
+        WHEN pi.rating > 8 THEN 'excellent'
         END AS rating,
     CASE
         WHEN pi.has_subtitles = TRUE THEN 'Bulgarian'
@@ -12,17 +12,19 @@ SELECT
     COUNT(pa.actor_id) AS actors_count
 FROM
     productions AS p
-LEFT JOIN
-    productions_info AS pi
-ON
-    p.production_info_id = pi.id
-LEFT JOIN
+JOIN
     productions_actors AS pa
 ON
     p.id = pa.production_id
+JOIN
+    productions_info AS pi
+ON
+    p.production_info_id = pi.id
 GROUP BY
-    p.title, pi.rating, pi.has_subtitles, pi.rating
+    p.title,
+    pi.rating,
+    pi.has_subtitles
 ORDER BY
     rating,
     actors_count DESC,
-    p.title;
+    p.title

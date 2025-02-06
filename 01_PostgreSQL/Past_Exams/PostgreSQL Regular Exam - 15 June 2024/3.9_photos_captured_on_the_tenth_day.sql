d719fcc277563cfd63e39e6b5924dfcd120e4e37
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-06 09:07:44 +0200

    Update 3.9_photos_captured_on_the_tenth_day.sql

--- 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/3.9_photos_captured_on_the_tenth_day.sql
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/3.9_photos_captured_on_the_tenth_day.sql
@@ -1,9 +1,21 @@
 SELECT
-    CONCAT(SUBSTRING(description, 1, 10), '...') AS summary,
-    TO_CHAR(capture_date, 'DD.MM HH24:MI') AS "date"
+    CONCAT(LEFT(p.description, 10), '...') AS summary,
+    TO_CHAR(p.capture_date, 'DD.MM HH24:MI' ) AS date
 FROM
     photos AS p
 WHERE
-    EXTRACT(DAY FROM capture_date) = 10
+    EXTRACT('DAY' FROM capture_date) = 10
 ORDER BY
     capture_date DESC
+;
+
+
+-- SELECT
+--     CONCAT(SUBSTRING(description, 1, 10), '...') AS summary,
+--     TO_CHAR(capture_date, 'DD.MM HH24:MI') AS "date"
+-- FROM
+--     photos AS p
+-- WHERE
+--     EXTRACT(DAY FROM capture_date) = 10
+-- ORDER BY
+--     capture_date DESC

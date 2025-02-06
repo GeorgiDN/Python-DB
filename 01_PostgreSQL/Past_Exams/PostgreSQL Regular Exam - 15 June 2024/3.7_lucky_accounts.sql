bfc082968f7e2d1e2f16580daac1184916fb1d38
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-06 08:22:11 +0200

    Update 3.7_lucky_accounts.sql

--- 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/3.7_lucky_accounts.sql
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/3.7_lucky_accounts.sql
@@ -1,17 +1,32 @@
 SELECT
     CONCAT(a.id, ' ', a.username) AS id_username,
-    a.email
+    a.email AS email
 FROM
     accounts AS a
 JOIN
     accounts_photos AS ap
 ON
     a.id = ap.account_id
-JOIN
-    photos AS p
-ON
-    ap.photo_id = p.id
 WHERE
-    a.id = ap.photo_id
+    ap.photo_id = ap.account_id
 ORDER BY
     a.id;
+
+
+-- SELECT
+--     CONCAT(a.id, ' ', a.username) AS id_username,
+--     a.email
+-- FROM
+--     accounts AS a
+-- JOIN
+--     accounts_photos AS ap
+-- ON
+--     a.id = ap.account_id
+-- JOIN
+--     photos AS p
+-- ON
+--     ap.photo_id = p.id
+-- WHERE
+--     a.id = ap.photo_id
+-- ORDER BY
+--     a.id;

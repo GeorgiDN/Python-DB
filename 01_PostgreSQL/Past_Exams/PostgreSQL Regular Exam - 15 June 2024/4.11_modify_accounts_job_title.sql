535804a336fe04994734c88c88155e0e193d3a6c
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-06 09:47:09 +0200

    Update 4.11_modify_accounts_job_title.sql

--- 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/4.11_modify_accounts_job_title.sql
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/4.11_modify_accounts_job_title.sql
@@ -1,39 +1,64 @@
-CREATE OR REPLACE PROCEDURE udp_modify_account(
+DROP PROCEDURE IF EXISTS udp_modify_account;
+CREATE PROCEDURE udp_modify_account(
     address_street VARCHAR(30),
     address_town VARCHAR(30)
-)
-AS
+) AS
 $$
 BEGIN
-    UPDATE accounts
-    SET job_title = CONCAT('(Remote) ', job_title)
-    WHERE id IN(
-        SELECT account_id
-        FROM addresses
-        WHERE address_street = street
-        AND town = address_town
-    );
-END;
+    UPDATE
+        accounts
+    SET
+        job_title = CONCAT('(Remote) ', job_title)
+    WHERE
+        id IN (
+            SELECT
+                account_id
+            FROM
+                addresses
+            WHERE
+                address_street = street
+            AND
+                address_town = town
+            )
+;
+END
 $$
 LANGUAGE plpgsql;
-
--- CALL udp_modify_account('97 Valley Edge Parkway', 'Nonexistent');
--- SELECT a.username, a.gender, a.job_title FROM accounts AS a
--- WHERE a.job_title ILIKE '(Remote)%';
 
 
 -- CALL udp_modify_account('97 Valley Edge Parkway', 'Divinópolis');
 -- SELECT a.username, a.gender, a.job_title FROM accounts AS a
 -- WHERE a.job_title ILIKE '(Remote)%';
 
+-- CALL udp_modify_account('97 Valley Edge Parkway', 'Nonexistent');
+-- SELECT a.username, a.gender, a.job_title FROM accounts AS a
+-- WHERE a.job_title ILIKE '(Remote)%';
 
 
 
+--------------------------------------------------------------------------------------------------------
+-- CREATE OR REPLACE PROCEDURE udp_modify_account(
+--     address_street VARCHAR(30),
+--     address_town VARCHAR(30)
+-- )
+-- AS
+-- $$
+-- BEGIN
+--     UPDATE accounts
+--     SET job_title = CONCAT('(Remote) ', job_title)
+--     WHERE id IN(
+--         SELECT account_id
+--         FROM addresses
+--         WHERE address_street = street
+--         AND town = address_town
+--     );
+-- END;
+-- $$
+-- LANGUAGE plpgsql;
 
 
 
-
-------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
 -- CREATE OR REPLACE PROCEDURE udp_modify_account(
 --     address_street VARCHAR(30),
 --     address_town VARCHAR(30)

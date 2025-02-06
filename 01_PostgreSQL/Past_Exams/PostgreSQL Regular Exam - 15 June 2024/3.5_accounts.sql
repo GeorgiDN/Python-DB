e26432bf61f75121a88b750851499d1caefd764d
Georgi Nikolov <140305579+GeorgiDN@users.noreply.github.com>
2025-02-06 07:52:12 +0200

    Update 3.5_accounts.sql

--- 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/3.5_accounts.sql
+++ 01_PostgreSQL/Past_Exams/PostgreSQL Regular Exam - 15 June 2024/3.5_accounts.sql
@@ -2,6 +2,13 @@ SELECT
     username,
     gender,
     age
-FROM accounts
-WHERE age >= 18 AND char_length(username) > 9
-ORDER BY age DESC, username
+FROM
+    accounts
+WHERE
+    (age >= 18)
+AND
+    CHAR_LENGTH(username) > 9
+ORDER BY
+    age DESC,
+    username
+;
